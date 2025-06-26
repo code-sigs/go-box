2b68584d8918ed1a2b7581b836debf90f337ba17
huangkun <https://xhs.szs.gd:7990/>
2025-06-26 14:01:40 +0800

    fixbugs

--- pkg/repository/base_repository.go
+++ pkg/repository/base_repository.go
@@ -19,7 +19,7 @@ type BaseRepository[T any, K comparable] interface {
 	List(ctx context.Context) ([]*T, error)
 	FindOne(ctx context.Context, filter map[string]any) (*T, error)
 	Find(ctx context.Context, filter map[string]any, sort map[string]int) ([]*T, error)
-	Paginate(ctx context.Context, offset int, limit int, filter map[string]any, sort map[string]int) ([]*T, int64, error)
+	Paginate(ctx context.Context, page int, limit int, filter map[string]any, sort map[string]int) ([]*T, int64, error)
 	Count(ctx context.Context, filter map[string]any) (int64, error)
 	WithTransaction(ctx context.Context, fn func(txCtx context.Context) error) error
 }
