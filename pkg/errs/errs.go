190acf67931f7c106ddc49b2a0899dfc70d8b536
huangkun <https://xhs.szs.gd:7990/>
2025-12-25 11:05:37 +0800

    add error code

--- pkg/errs/errs.go
+++ pkg/errs/errs.go
@@ -1,8 +1,11 @@
 package errs
 
 const (
-	ErrorInternal     = 500000
-	ErrorArgs         = 500001
-	ErrorNoPermission = 500002
-	ErrorNoFound      = 500003
+	ErrorInternal     = 500000 //系统异常
+	ErrorArgs         = 500001 //参数错误
+	ErrorNotFound     = 500002 //记录不存在
+	ErrorNoPermission = 500004 //无操作权限
+	ErrorNoUser       = 500005 //用户不存在
+	ErrorPassword     = 500006 //密码错误
+	ErrorInvalidToken = 500007 //无效token
 )
